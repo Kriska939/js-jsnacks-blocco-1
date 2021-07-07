6e4995ce923e97c31035f88b697f58da3a04cf2f
Kriska939 <cristinacorrado93@gmail.com>
2021-07-07 16:42:12 +0200

    added basic logic snack2

--- js/snack2.js
+++ js/snack2.js
@@ -4,3 +4,13 @@ SNACK 2: Chiedi un numero all'utente, se è pari stampa il numero,
 se è dispari stampa il numero successivo.
 
 */
+
+var resultNumber = document.getElementById("result");
+
+var userNumber = parseInt(prompt("Choose a number", 1));
+
+if (userNumber % 2 === 0) {
+    resultNumber.innerText = userNumber;
+} else {
+    resultNumber.innerText = userNumber + 1;
+}
