033564705960ec84f8129ca5384789bfdc1d4da8
Kriska939 <cristinacorrado93@gmail.com>
2021-07-07 16:17:43 +0200

    added solution "for"

--- js/snack1.js
+++ js/snack1.js
@@ -5,3 +5,26 @@ Il programma stampa la somma di tutti i numeri inseriti.
 Esegui questo programma in due versioni, con il for e con il while.
 
 */
+
+
+// versione for
+
+var somma = 0;
+
+// for (var i = 0; i < 5; i++) {
+//     var userNumber = parseInt(prompt("Choose a number", 1));
+
+    //check numeri inseriti
+//     console.log(userNumber);
+
+//     if (!isNaN(userNumber)) {
+//         somma += userNumber;
+//     }
+// }
+
+// check somma
+// console.log(somma);
+
+
+// versione while
+
