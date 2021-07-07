64b1bc7f0ac8dd86ce9d85dc32990e566b45e2ab
Kriska939 <cristinacorrado93@gmail.com>
2021-07-07 16:54:29 +0200

    added basic values snack 3

--- js/snack3.js
+++ js/snack3.js
@@ -3,3 +3,12 @@
 SNACK 3: Generatore di “nomi cognomi” casuali: prendendo una lista di nomi e una lista di cognomi, Gatsby vuole generare una falsa lista di 3 invitati.
 
 */
+
+// Array di base + lista vuota:
+
+var firstNameList = ["Marco", "Paolo", "Arianna", "Marika", "Luisa", "Teresa"];
+var lastnameList = ["Rossi", "Verdi", "Bianchi", "Russo", "Ricci", "Esposito", "Romano"];
+var finalList = [];
+
+
+
